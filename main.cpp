56430415a8f743344f202b0447dde2326b217f01
Paul Catalan <contact.pcatl@gmail.com>
2019-12-03 13:28:32 +0800

    Updated indent; Added findDialog function.

--- main.cpp
+++ main.cpp
@@ -3,70 +3,117 @@
 #include <iostream>
 
 struct Dialog;
-typedef std::vector<std::pair<Dialog*, std::string>> choices_t;
+typedef std::vector<std::pair<const Dialog*, std::string>> dialog_options_t;
 
 struct Dialog
 {
-	Dialog(std::string text, choices_t choices)
-	: text{text}
-	, choices{choices}
-	{
-	}
+    Dialog(std::string text, dialog_options_t choices)
+    : text{text}
+    , choices{choices}
+    {
+    }
 
-	std::string text;
-	choices_t choices;
+    std::string text;
+    dialog_options_t choices;
 };
 
 struct Conversation
 {
-	std::vector<Dialog*> dialogs;
+    std::vector<Dialog*> dialogs;
 
-	Conversation()
-	: dialogs{}
-	{
-		createDialogs();
-	}
+    Conversation()
+    : dialogs{}
+    {
+        createDialogs();
+    }
 
-	void createDialogs()
-	{
-		dialogs.push_back(new Dialog("Root!", choices_t{{nullptr, ""}}));
-		dialogs.push_back(
-			new Dialog(
-				"Previous!",
-					choices_t (
-						{
-							{dialogs.at(0), "Go to root."},
-							{dialogs.at(0), "Dont go to root."}
-						}
-					)
-				)
-		);
-	}
+    const Dialog* findDialog(std::string text) const
+    {
+        for (auto& dialog : dialogs)
+        {
+            if (text == dialog->text)
+            {
+                return dialog;
+            }
+        }
+        return new Dialog("", dialog_options_t{});
+    }
 
-	void showDialogs()
-	{
-		auto currentDialog = dialogs.at(1);
+    void createDialogs()
+    {
+        dialogs.push_back(new Dialog("Root!", dialog_options_t{}));
+        dialogs.push_back(new Dialog("Trap!", dialog_options_t{{nullptr, "Your are trapped!"}}));
+        dialogs.push_back(
+            new Dialog(
+                "Previous!",
+                dialog_options_t (
+                    {
+                        {findDialog("Root!"), "Go to root."},
+                        {findDialog("Trap!"), "Go to an adventure!"},
+                        {nullptr, "Dont go to root."}
+                    }
+                )
+                )
+        );
+    }
 
-		while (currentDialog)
-		{
-			std::cout << "\n\n" << currentDialog->text;
+    void showDialogs() const
+    {
+        auto currentDialog = findDialog("Previous!");
 
-			for (auto i = 0u; i < currentDialog->choices.size(); ++i)
-			{
-				std::cout << "\n" << (i+1) << ". " << currentDialog->choices.at(i).second;
-			}
+        while (currentDialog)
+        {
+            std::cout << "\n\n" << currentDialog->text << "\n";
 
-			currentDialog = currentDialog->choices.at(0).first;
-		}
-	}
+            if (currentDialog->choices.size())
+            {
+                for (auto i = 0u; i < currentDialog->choices.size(); ++i)
+                {
+                    std::cout << "\n" << (i+1) << ". " << currentDialog->choices.at(i).second;
+                }
 
+                auto choice = getInput();
+                currentDialog = currentDialog->choices.at(choice).first;
+
+                continue;
+            }
+            currentDialog = nullptr;
+        }
+    }
+
+    const uint8_t getInput() const
+    {
+        uint8_t input = 0;
+
+        std::cout << "\n== Enter your choice: ";
+        // std::cin >> input;
+
+        return input;
+    }
+
+};
+
+class Game
+{
+public:
+    Game()
+    : convo{}
+    {
+    }
+
+    void start()
+    {
+        convo.showDialogs();
+    }
+private:
+    Conversation convo;
 };
 
 int main()
 {
-	Conversation c;
-	c.showDialogs();
+    Game g{};
+    g.start();
 
 
-	return 0;
+    return 0;
 }
---
+++ run.bat
@@ -0,0 +1,1 @@
+g++ main.cpp -std=c++14 -o  main.exe && main.exe
